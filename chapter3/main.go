d2e12f98ef8fcacae47c8b7515ea37d1876bac6a
Sacha <rockelov@gmail.com>
2020-07-29 22:13:42 +0700

    random distance

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	const hoursPerDay = 24
-	var distance = 96300000                                    // km
-	var speed = 100800                                         // km/h
-	fmt.Printf("Days to Mars: %v", distance/speed/hoursPerDay) // 39
+	var distance = rand.Intn(401000000-56000000+1) + 5600000 // km 96300000
+	var speed = 100800                                       // km/h
+	fmt.Printf("Distance to mars: %v km\nDays to Mars: %v", distance, distance/speed/hoursPerDay)
 }
