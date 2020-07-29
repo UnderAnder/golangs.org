af45efde2d86533b350ef15d5e2395e5561551cf
Sacha <rockelov@gmail.com>
2020-07-29 22:31:35 +0700

    chapter3 complete

--- chapter3/main.go
+++ chapter3/main.go
@@ -9,5 +9,5 @@ func main() {
 	const hoursPerDay = 24
 	var distance = rand.Intn(401000000-56000000+1) + 5600000 // km 96300000
 	var speed = 100800                                       // km/h
-	fmt.Printf("Distance to mars: %v km\nDays to Mars: %v", distance, distance/speed/hoursPerDay)
+	fmt.Printf("Distance to mars: %v km\nDays to Mars: %v\n28day speed: %v km/h", distance, distance/speed/hoursPerDay, distance/(28*hoursPerDay))
 }
