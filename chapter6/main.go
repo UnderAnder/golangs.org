49dde347eb8eaa7d3c28742bf0069c1c07c3e78a
Sacha <rockelov@gmail.com>
2020-07-31 21:04:10 +0700

    chapter 6 complete

--- chapter6/main.go
+++ chapter6/main.go
@@ -6,22 +6,24 @@ import (
 )
 
 func main() {
-	var spaceline = "SpaceX"
-	var triptype bool
-
+	var spaceline = [3]string{"SpaceX", "Space Adventures", "Virgin Galactic"}
 	var distance = 62100000 //км
 
-	fmt.Printf("Spaceline Days Trip Type Price\n")
+	fmt.Printf("Spaceline         Days Trip Type Price\n")
 	for count := 0; count < 10; count++ {
+		var triptype string
 		var k = rand.Intn(15)
 		var shipspeed = k + 16                                   //км/с
 		var flightduration = distance / shipspeed / 60 / 60 / 24 // days
 		var price = k*1000000 + 36000000
-		if triptype {
-			price *= 2
+		if rand.Intn(2) == 1 {
+			triptype = "Round-trip"
+			price = price * 2
+		} else {
+			triptype = "One-way"
 		}
-		fmt.Printf("%v %v %v %v", spaceline, flightduration, triptype, price)
-		fmt.Println(count)
+
+		fmt.Printf("%-16v %4v %-10v $%4v\n", spaceline[rand.Intn(3)], flightduration, triptype, price)
 	}
 
 }
