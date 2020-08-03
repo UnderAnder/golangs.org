d1ab1381ed9e97be78d1b26d6d87262bdc27d289
Sacha <rockelov@gmail.com>
2020-08-03 15:49:39 +0700

    chap12 ex2 complete

--- chapter12/main.go
+++ chapter12/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -9,22 +10,8 @@ func main() {
 	keyword := "GOLANG"
 	word := ""
 
-	/* 	for i, c := range cipherText {
-	   		if c >= 'A' && c <= 'Z' {
-	   			if i < len(keyword) {
-	   				c = c - rune(keyword[i]) // 'G'
-	   				if c < 'A' {
-	   					c += 26
-	   				}
-	   			} else {
-	   				i -= len(keyword)
-	   			}
-	   		}
-	   		fmt.Printf("%c", c)
-	   	}
-	*/
 	for i := 0; i < len(cipherText); i++ {
-		// 'A' = 1, 'Z' = 26
+		// 'A' = 0, 'Z' = 25
 
 		c := cipherText[i%len(cipherText)] - (keyword[i%len(keyword)] - 'A')
 		if c < 'A' {
@@ -35,4 +22,29 @@ func main() {
 		word += string(c)
 	}
 	fmt.Println(word)
+
+	message := "your message goes here"
+	result := ""
+	keyIndex := 0
+	message = strings.Replace(message, " ", "", -1)
+	message = strings.ToUpper(message)
+	for i := 0; i < len(message); i++ {
+		c := message[i]
+		if c >= 'A' && c <= 'Z' {
+			// A=0, B=1, ... Z=25
+			c -= 'A'
+			k := keyword[keyIndex] - 'A'
+
+			// зашифрованная буква + ключевая буква
+			c = (c+k)%26 + 'A'
+
+			// увеличить keyIndex
+			keyIndex++
+			keyIndex %= len(keyword)
+		}
+		result += string(c)
+	}
+
+	fmt.Println(result)
+
 }
