e8c418d13e340067bfc407c626adaee6becef0f1
Sacha <rockelov@gmail.com>
2020-08-04 12:18:03 +0700

    chap16 complete

--- chapter16/main.go
+++ chapter16/main.go
@@ -14,15 +14,16 @@ func (f fahrenheit) celsius() celsius {
 	return celsius((f - 32.0) * 5.0 / 9.0)
 }
 
-type sensor func(t celsius) (string, string)
+//type sensor func(t celsius) (string, string)
+type sensor func(t int) (string, string)
 
-func drawTable(header1, header2 string, tempMin, tempMax, step celsius, s sensor) {
+func drawTable(header1, header2 string, tempMin, tempMax, step int, s sensor) {
 	line := "===============\n"
 	pattern := "|%6v|%6v|\n"
 	fmt.Printf(line)
 	fmt.Printf(pattern, header1, header2)
 	rows := (tempMax - tempMin) / step
-	for row := 0; row <= int(rows); row++ {
+	for row := 0; row <= rows; row++ {
 		cell1, cell2 := s(tempMin)
 		fmt.Printf(pattern, cell1, cell2)
 		tempMin += step
@@ -30,20 +31,20 @@ func drawTable(header1, header2 string, tempMin, tempMax, step celsius, s sensor
 	fmt.Printf(line)
 }
 
-func ctof(temp celsius) (string, string) {
+func ctof(temp int) (string, string) {
 	cell1 := fmt.Sprintf("%v", temp)
-	cell2 := fmt.Sprintf("%v", temp.fahrenheit())
+	cell2 := fmt.Sprintf("%v", celsius(temp).fahrenheit())
 	return cell1, cell2
 }
 
-func ftoc(temp celsius) (string, string) {
-	cell1 := fmt.Sprintf("%v", temp.fahrenheit())
-	cell2 := fmt.Sprintf("%v", temp)
+func ftoc(temp int) (string, string) {
+	cell1 := fmt.Sprintf("%v", fahrenheit(temp))
+	cell2 := fmt.Sprintf("%6.2f", fahrenheit(temp).celsius())
 	return cell1, cell2
 }
 
 func main() {
-	var tempMin, tempMax, step celsius = -40, 100, 5
+	var tempMin, tempMax, step int = -40, 100, 5
 	drawTable("°C", "°F", tempMin, tempMax, step, ctof)
 	drawTable("°F", "°C", tempMin, tempMax, step, ftoc)
 }
