ff3baffea87c5b3ca52f478bb02c456fcc8387f1
Sacha <rockelov@gmail.com>
2020-08-04 18:59:34 +0700

    chapter20 complete

--- chapter20/main.go
+++ chapter20/main.go
@@ -7,10 +7,20 @@ import (
 
 func listOfWords(text string) []string {
 	text = strings.ToLower(text)
-	text = strings.Trim(text, ".,!;")
+	text = strings.Trim(text, ";.,!:")
 	return strings.Fields(text)
 }
 
+/* func countWords(text string) map[string]int {
+    words := strings.Fields(strings.ToLower(text))
+    frequency := make(map[string]int, len(words))
+    for _, word := range words {
+        word = strings.Trim(word, `.,"-`)
+        frequency[word]++
+    }
+    return frequency
+}
+ */
 func main() {
 	text := "As far as eye could reach he saw nothing but the stems of the great plants about him receding in the violet shade, and far overhead the multiple transparency of huge leaves filtering the sunshine to the solemn splendour of twilight in which he walked. Whenever he felt able he ran again; the ground continued soft and springy, covered with the same resilient weed which was the first thing his hands had touched in Malacandra. Once or twice a small red creature scuttled across his path, but otherwise there seemed to be no life stirring in the wood; nothing to fear—except the fact of wandering unprovisioned and alone in a forest of unknown vegetation thousands or millions of miles beyond the reach or knowledge of man."
 
@@ -21,7 +31,8 @@ func main() {
 	}
 
 	for t, num := range frequency { // Итерирует через карту (ключ, значение)
-		fmt.Printf("%v встречается %d раз(а) \n", t, num)
+		if num > 1 {
+			fmt.Printf("%v встречается %d раз(а) \n", t, num)
+		}
 	}
-
 }
