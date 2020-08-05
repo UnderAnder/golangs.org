6f37dae880da4e1b04cc92112c5eda3204b621a5
Sacha <rockelov@gmail.com>
2020-08-05 10:25:51 +0700

    chapter21 keep working

--- chapter21/main.go
+++ chapter21/main.go
@@ -53,12 +53,47 @@ func (u Universe) Neighbors(x, y int) int {
 	count := 0
 	for i := -1; i < 2; i++ {
 		for k := -1; k < 2; k++ {
-			if u.Alive(x+i, y+k) {
+			if !(i == 0 && k == 0) && u.Alive(x+i, y+k) {
 				count++
 			}
 		}
 	}
 	return count
+}
+
+func (u Universe) Next(x, y int) bool {
+	switch u.Neighbors(x, y) {
+	case 0, 1:
+		return false
+	case 2:
+		return true
+	case 3:
+		if u.Alive(x, y) {
+			return false
+		} else {
+			return true
+		}
+	default:
+		return false
+	}
+}
+
+func Step(a, b Universe) {
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			a.Neighbors(i,j)
+			b.Next(i, j)
+		}
+	}
+
+	a, b = b, a
+
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			a.Neighbors(i,j)
+			b.Next(i, j)
+		}
+	}
 }
 
 func NewUniverse() Universe {
@@ -76,5 +111,6 @@ func main() {
 	world.Seed()
 	world.Show()
 
-	fmt.Println(world.Neighbors(3, 3))
+	fmt.Println(world.Neighbors(5, 5))
+	fmt.Println(world.Next(5, 5))
 }
